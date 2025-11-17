ac3b747f8950e82183aa44272dd56953f643e498
tool987 <iammandefroabebaw@gmail.com>
2025-11-17 06:46:53 +0300

    advanced update the account

--- ecommerce/Apps/accounts/admin.py
+++ ecommerce/Apps/accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Profile, BankInfo
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+
+
+    fieldsets = (
+    (None, {'fields': ('email', 'password')}),
+    (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+    (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    (_('Important dates'), {'fields': ('date_joined',)}),
+)
+
+
+
+
+@admin.register(BankInfo)
+class BankInfoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bank_name', 'account_number', 'is_verified')
+    search_fields = ('user__email', 'bank_name', 'account_number')
+
